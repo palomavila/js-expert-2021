1a4cda7db80d15efef0eaa5e9342e3c20c89151a
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-20 16:30:33 -0300

    Update index.js

--- js-exercicios/src/index.js
+++ js-exercicios/src/index.js
@@ -1,7 +1,7 @@
-const assert = require('assert')
-const Employee = require('./employee')
-const Manager = require('./manager')
-const Util = require('./util')
+const assert = require('assert');
+const Employee = require('./employee');
+const Manager = require('./manager');
+const Util = require('./util');
 
 const GENDER = {
   male: 'male',
@@ -27,28 +27,28 @@ Date.prototype.getFullYear = () => CURRENT_YEAR;
     gender: GENDER.male
   })
 
-  assert.deepStrictEqual(employee.name, 'Mr. Lilo')
-  assert.deepStrictEqual(employee.age, undefined)
-  assert.deepStrictEqual(employee.gender, undefined)
-  assert.deepStrictEqual(employee.gender, undefined)
-  assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.40))
-  assert.deepStrictEqual(employee.netPay, Util.formatCurrency(4000.32))
+  assert.deepStrictEqual(employee.name, 'Mr. Lilo');
+  assert.deepStrictEqual(employee.age, undefined);
+  assert.deepStrictEqual(employee.gender, undefined);
+  assert.deepStrictEqual(employee.gender, undefined);
+  assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.40));
+  assert.deepStrictEqual(employee.netPay, Util.formatCurrency(4000.32));
 
-  const expectedBirthYear = 1995
-  assert.deepStrictEqual(employee.birthYear, expectedBirthYear)
+  const expectedBirthYear = 1995;
+  assert.deepStrictEqual(employee.birthYear, expectedBirthYear);
  
   /* tem set*/
-  employee.age = 80
-  assert.deepStrictEqual(employee.birthYear, 1941)
+  employee.age = 80;
+  assert.deepStrictEqual(employee.birthYear, 1941);
     
 
-    console.log('\n----employee----')
-    console.log('employee.name', employee.name)
-    console.log('employee.gender', employee.gender)
-    console.log('employee.age', employee.age)
-    console.log('employee.birthYear', employee.birthYear)
-    console.log('employee.grossPay', employee.grossPay)
-    console.log('employee.netPay', employee.netPay)
+    console.log('\n----employee----');
+    console.log('employee.name', employee.name);
+    console.log('employee.gender', employee.gender);
+    console.log('employee.age', employee.age);
+    console.log('employee.birthYear', employee.birthYear);
+    console.log('employee.grossPay', employee.grossPay);
+    console.log('employee.netPay', employee.netPay);
 
 }
 
@@ -59,20 +59,20 @@ Date.prototype.getFullYear = () => CURRENT_YEAR;
     gender: GENDER.female
   })
 
-    assert.deepStrictEqual(manager.name, "Mr. Juninho")
-    assert.deepStrictEqual(manager.gender, undefined)
-    assert.deepStrictEqual(manager.age, undefined)
-    assert.deepStrictEqual(manager.birthYear, 1996)
-    assert.deepStrictEqual(manager.grossPay, Util.formatCurrency(5000.40))
-    assert.deepStrictEqual(manager.netPay, Util.formatCurrency(6000.32))
-    assert.deepStrictEqual(manager.bonuses, Util.formatCurrency(6000.32)
+    assert.deepStrictEqual(manager.name, "Mr. Juninho");
+    assert.deepStrictEqual(manager.gender, undefined);
+    assert.deepStrictEqual(manager.age, undefined);
+    assert.deepStrictEqual(manager.birthYear, 1996);
+    assert.deepStrictEqual(manager.grossPay, Util.formatCurrency(5000.40));
+    assert.deepStrictEqual(manager.netPay, Util.formatCurrency(6000.32));
+    assert.deepStrictEqual(manager.bonuses, Util.formatCurrency(6000.32);
 
-    console.log('\n----manager----')
-    console.log('manager.name', manager.name)
-    console.log('manager.gender', manager.gender)
-    console.log('manager.age', manager.age)
-    console.log('manager.birthYear', manager.birthYear)
-    console.log('manager.grossPay', manager.grossPay)
-    console.log('manager.netPay', manager.netPay)
-    console.log('manager.bonuses', manager.bonuses)
+    console.log('\n----manager----');
+    console.log('manager.name', manager.name);
+    console.log('manager.gender', manager.gender);
+    console.log('manager.age', manager.age);
+    console.log('manager.birthYear', manager.birthYear);
+    console.log('manager.grossPay', manager.grossPay);
+    console.log('manager.netPay', manager.netPay);
+    console.log('manager.bonuses', manager.bonuses);
   }
