c6d7a442ce8a9fccb4f8fa1a9886707c62dcb610
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-20 16:27:01 -0300

    Update entityBase.js

--- js-exercicios/src/entityBase.js
+++ js-exercicios/src/entityBase.js
@@ -1,29 +1,30 @@
 class EntityBase {
-    #name
-    #gender
-    #age
+    #name;
+    #gender;
+    #age;
     constructor({ name, age, gender }) {
 
-        this.#gender = gender
-        this.#age = age
-        this.#name = name
+        this.#gender = gender;
+        this.#age = age;
+        this.#name = name;
     }
 
     get name() {
-        const preffix = this.#gender === "male" ? "Mr." : "Ms."
-        return `${preffix} ${this.#name}`
+        const preffix = this.#gender === "male" ? "Mr." : "Ms.";
+        return `${preffix} ${this.#name}`;
     }
 
     get birthYear() {
-        if (!this.#age) throw new Error('you must define age first!')
-
-        return new Date().getFullYear() - this.#age
+      if (!this.#age) {
+       throw new Error('you must define age first!!');
+     }
+       return new Date().getFullYear() - this.#age;
     }
 
     set age(value) {
-        this.#age = value
+        this.#age = value;
     }
 
 }
 
-module.exports = EntityBase
+module.exports = EntityBase;
