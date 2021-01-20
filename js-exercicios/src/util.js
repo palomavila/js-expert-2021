04444c2827db105087b5b620d6890e2210322bb1
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-20 16:38:18 -0300

    Update util.js

--- js-exercicios/src/util.js
+++ js-exercicios/src/util.js
@@ -2,15 +2,16 @@ class Util {
     static #defaultFormart = Intl.NumberFormat("pt-br", {
         currency: "BRL",
         style: "currency"
-    })
+    });
 
     static formatCurrency(value) {
-        return this.#defaultFormart.format(value)
+        return this.#defaultFormart.format(value);
     }
+
     static unFormartCurrency(value) {
         return Number(value.replace(/\D/g, '')) / 100;
         
     }
 }
 
-module.exports = Util
+module.exports = Util;
